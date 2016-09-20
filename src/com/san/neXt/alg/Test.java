3ac854bd4f9c7552506c88bf0913656f30fa0d26
vasanta111 <svasanta.tez@gmail.com>
2016-09-20 10:27:19 -0700

    first set

---
+++ src/com/san/neXt/alg/Test.java
@@ -0,0 +1,11 @@
+package com.san.neXt.alg;
+
+/**
+ * Created by vasantas on 9/20/2016.
+ */
+public class Test {
+    public static void main(String args){
+        System.out.println("This is fine...");
+    }
+
+}
